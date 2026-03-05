0e37caa2a14e72e2dc77834bb52c063bda6a3928
DevSlavv <kozakcrm@gmail.com>
2026-03-05 23:10:48 +0200

    Delete binotel_integration directory

--- binotel_integration/config/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-defined('BASEPATH') or exit('No direct script access allowed');
-
-//$route['admin/binotel_admin/(:any)'] = 'binotel_integration/binotel_admin/$1';
-//$route['admin/binotel_admin'] = 'binotel_integration/binotel_admin/index';
-
-
--- binotel_integration/views/binotel_integration_view.php
+++
@@ -1,5 +0,0 @@
-<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
-
-
-<h1>Binotel Integration</h1>
-<p>Цей модуль інтегрує Binotel з Perfex CRM.</p>
